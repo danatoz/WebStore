a7ed436d6fdfc5cb704c606ae161c94f473a15c5
Edward <gulyaev.ed.v@gmail.com>
2020-08-27 23:53:42 +0400

    Lesson_2 - Added web interface

---
+++ WebStore/Controllers/EmployeeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using WebStore.Models;
+
+namespace WebStore.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        private readonly List<EmployeeView> _employees = new List<EmployeeView>
+        {
+            new EmployeeView
+            {
+                Id = 1,
+                FirstName = "Иван" ,
+                SurName = "Иванов" ,
+                Patronymic = "Иванович" ,
+                Age = 45,
+                Position = "Директор"
+            },
+            new EmployeeView
+            {
+                Id = 2,
+                FirstName = "Владислав" ,
+                SurName = "Петров" ,
+                Patronymic = "Иванович" ,
+                Age = 22,
+                Position = "Заместитель директора"
+            }
+        };
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Employees()
+        {
+            return View(_employees);
+        }
+        public IActionResult EmployeeDetails(int id)
+        {
+            //Получаем сотродника по Id
+            var employee = _employees.FirstOrDefault(t => t.Id == id);
+            //Проверка на существует ли данный сотрудник
+            if (employee == null)
+                return NotFound();//404 NotFound
+            //Возвращаем сотрудника
+            return View(employee);
+        }
+    }
+}
