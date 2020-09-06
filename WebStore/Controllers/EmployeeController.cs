b2120a271600f264a3aa39e572759e0226bc5905
Edward <gulyaev.ed.v@gmail.com>
2020-09-06 14:30:12 +0400

    Серверная валидация

--- WebStore/Controllers/EmployeeController.cs
+++ WebStore/Controllers/EmployeeController.cs
@@ -36,6 +36,10 @@ namespace WebStore.Controllers
         [Route("edit/{id?}")]
         public IActionResult Edit(EmployeeView model)
         {
+            if(model.Age < 18 || model.Age > 100)
+                ModelState.AddModelError("Age", "Ошибка возраста!");
+            if (!ModelState.IsValid)
+                return View(model);
             if (model.Id > 0)
             {
                 var dbItem = _employeesService.GetById(model.Id);
--- WebStore/Models/EmployeeView.cs
+++ WebStore/Models/EmployeeView.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,10 +9,19 @@ namespace WebStore.Models
     public class EmployeeView
     {
         public int Id { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Имя является обязательным")]
+        [Display(Name = "Имя")]
+        [StringLength(200,MinimumLength = 2, ErrorMessage = "В имени может быть не менее 2-х символов и не более 200")]
         public string FirstName { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Фамилия является обязательной")]
+        [Display(Name = "Фамилия")]
+        [StringLength(200, MinimumLength = 2, ErrorMessage = "В фамилии может быть не менее 2-х символов и не более 200")]
         public string SurName { get; set; }
+        [Display(Name = "Отчество")]
         public string Patronymic { get; set; }
+        [Display(Name = "Возраст")]
         public int Age { get; set; }
+        [Display(Name = "Должность")]
         public string Position { get; set; }
     }
 }
